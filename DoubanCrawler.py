a89daaf8ff5d86363dde6eaa80d9c17b214a88ef
Hanbei <15510320767@163.com>
2018-02-06 13:15:59 +0800

    add Movie class

--- DoubanCrawler.py
+++ DoubanCrawler.py
@@ -11,3 +11,23 @@ def getMovieUrl(category, location):
 url = getMovieUrl("电影","日本")
 print(url)
 html = expanddouban.getHtml(url)
+
+class Movie(object):
+	"""
+	Movie class contains:
+	Name, rate, category, location, page link, post link
+	"""
+	name = ""
+	rate = 0
+	category = ""
+	location = ""
+	page_link = ""
+	post_link = ""
+	def __init__(self, name, rate, category, location, page_link, post_link):
+		super(Movie, self).__init__()
+		self.name = name
+		self.rate = rate
+		self.category = category
+		self.location = location
+		self.page_link = page_link
+		self.post_link = post_link
