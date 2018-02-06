e1c43b8d02c0274a130340796163b7c7b921c20a
Hanbei <15510320767@163.com>
2018-02-06 16:41:28 +0800

    write movies into csv

--- DoubanCrawler.py
+++ DoubanCrawler.py
@@ -1,6 +1,8 @@
 from urllib.parse import urljoin
 import expanddouban
 from bs4 import BeautifulSoup
+import csv
+import codecs
 """
 return a string corresponding to the URL of douban movie lists given category and location.
 """
@@ -29,6 +31,7 @@ class Movie(object):
 		self.img_link = img_link
 
 def getMovies(category, location):
+	movies = []
 	"""
 	return a list of Movie objects with the given category and location.
 	"""
@@ -37,9 +40,20 @@ def getMovies(category, location):
 	"""find the movie list in soup"""
 	list_div = soup.find(attrs={'data-v-3e982be2':''}).find(class_="list-wp")
 	items = list_div.find_all(class_="item")
+	"""get data of each movie"""
 	for item in items:
 		name = item.p.find(class_='title').get_text().strip()
 		rate = item.p.find(class_='rate').get_text().strip()
 		page_link = item.get('href')
 		img_link = item.find(class_="pic").img.get('src')
-	return []
+		movies.append(Movie(name, rate, category, location, page_link, img_link))
+	return movies
+
+"""write movies into csv"""
+movies = getMovies("电影","日本")
+with open('movies.csv', 'w', encoding='gb18030') as csvfile:
+	#configure writer to write standard csv file
+	writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+	writer.writerow(['name', 'rate', 'category', 'location', 'page_link', 'img_link'])
+	for movie in movies:
+		writer.writerow([movie.name, movie.rate, movie.category, movie.location, movie.page_link, movie.img_link])
